67d3287faac044a7f3d71856c180804ba9346b55
Muchammad Muchib Zainul Fikry <muchammadmuchib@gmail.com>
2026-01-28 10:58:27 +0700

    pdf merge

--- schema.sql
+++ schema.sql
@@ -12,6 +12,30 @@ CREATE TABLE IF NOT EXISTS pdf_files (
     deleted_at TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS roles (
+    id BIGSERIAL PRIMARY KEY,
+    role VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    address VARCHAR(255),
+    created_by BIGINT,
+    created_date TIMESTAMP,
+    email VARCHAR(30) UNIQUE NOT NULL,
+    is_email_verified BOOLEAN DEFAULT FALSE,
+    modified_by BIGINT,
+    modified_date TIMESTAMP,
+    name VARCHAR(50),
+    password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(13),
+    post_code CHAR(5),
+    role_id BIGINT REFERENCES roles(id)
+);
+
+-- Seed Initial Roles
+INSERT INTO roles (role) VALUES ('Project Manager'), ('Financial'), ('HRD') ON CONFLICT DO NOTHING;
+
 -- Indexes (Optional for performance)
 CREATE INDEX idx_status ON pdf_files(status);
 CREATE INDEX idx_created_at ON pdf_files(created_at);
